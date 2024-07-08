bbbaf15da0e5b19855af16758f311f7c6aac09c4
Ian Nakamura <16380267788@estudante.ufjf.br>
2024-07-08 13:53:48 -0300

    Edição inicial de Index.js e Conn.js

---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require ('sequelize')
+
+const sequelize = new Sequelize('soraya', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+
+try{
+    sequelize.authenticate()
+    console.log('Conectamos no banco com sucesso')
+
+} catch(err) {
+    console.log(`Não foi possível conectar: ${err}`)
+}
+
+module.exports = sequelize
